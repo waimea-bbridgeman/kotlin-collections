456a509c2af308fa941015008c61487b62e35ae9
waimea-bbridgeman <brbridgeman@waimea.school.nz>
2025-03-18 14:48:14 +1300

    update

--- src/Main.kt
+++ src/Main.kt
@@ -65,7 +65,65 @@ fun main() {
         println("$i: $snack")
     }
 
-    
+
+//    const val EXIT = 'x'
+//    val ADD = 'a'
+//
+//    fun main() {
+//        println("Accounting System 5000")
+//        println()
+//
+//        val accounts = mutableListOf<Double>()
+//
+//        //Test values
+//        accounts.add(3.14)
+//        accounts.add(2.81)
+//        accounts.add(7.905)
+//        accounts.add(12.97)
+//        accounts.add(195.016)
+//
+//        showAccounts(accounts)
+//
+//        while (true) {
+//            val action = getUserAction()
+//
+//            when (action) {
+//
+//                EXIT -> break
+//
+//                //User wants to add new value
+//                ADD -> {}
+//            }
+//        }
+//    }
+//
+//    fun showAccounts(accounts: List<Double>) {
+//        var sum = 0.0
+//        //Show the values in the list
+//        for (value in accounts) {
+//            println("%8.2f".format(value))
+//            sum += value
+//        }
+//    }
+//
+//    fun getUserAction(): Char  { //Char returns a single letter or single value
+//        //Show a menu of options and get users choice and you will return the choice
+//        return EXIT
+//
+//    }
+//
+//    fun average(average: Array<String>) {
+//        val numArray = doubleArrayOf(45.3, 67.5, -45.6, 20.34, 33.0, 45.6)
+//        var sum = 0.0
+//
+//        for (num in numArray) {
+//            sum += num
+//        }
+//
+//        val average = sum / numArray.size
+//        println("The average is: %.2f".format(average))
+//    }
+
 
 
 
